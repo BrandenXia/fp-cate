83e5218cb1168cb8cf397c1efe73f1161c0d5fbd
BrandenXia <xxtbranden@outlook.com>
2025-09-16 21:56:56 -0700

    revert!: delete unuseful stuff

--- fp_cate/__init__.py
+++ fp_cate/__init__.py
@@ -1,5 +1,3 @@
-from .type_class import Tp
-from .type_classes import Semigroup, Functor
 from .utils import (
     identity,
     assert_and,
@@ -13,17 +11,12 @@ from .utils import (
 )
 from .pattern_match import case, default, matchV, match, _any, _rest
 
-lst = Tp([Functor, Semigroup], [1, 2, 3])
-
 # fmt: off
 __all__ = [
-    # type_classes
-    "Semigroup", "Functor",
     # utils
     "identity", "assert_and", "expr", "compose", "curry", "cache", "pipe",
     "Filter", "State",
     # pattern_match
     "case", "matchV", "match", "_any", "_rest", "default",
-    "lst"
 ]
 # fmt: on
