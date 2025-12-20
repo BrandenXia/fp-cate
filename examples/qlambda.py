d6297980dcf9b2231ca8ba3a7818f311c2522d06
BrandenXia <xxtbranden@outlook.com>
2025-12-19 18:27:39 -0800

    docs: some more examples for qlambda

--- examples/qlambda.py
+++ examples/qlambda.py
@@ -6,3 +6,6 @@ def test(a):
 
 
 print((_1 + _2 + test(_2))(9, 2))
+print(sorted([(4, 3), (1, 2), (2, 1)], key=_1[0]))
+print(sorted([(4, 3), (1, 2), (2, 1)], key=_1[1]))
+print(len(list(filter(_1 % 2 == 0, range(10)))))
